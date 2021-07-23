be0f48af92fff1b819dc798ebaa121203d2589d7
RazdolbayOne <Yone312@gmail.com>
2021-07-23 19:11:28 +0300

    some skice for functions

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python3
+
+import requests
+
+
+def post_request():
+    """ Rest POST request to BANK API to get back as response
+    json file what contains acces token"""
+    pass
+
+
+def get_request() -> str:
+    """Rest GET Call for authification to BANK API
+
+    :returns html page"""
+    pass
+
+
 def main():
     print("Hello World of SEB API!")
+    r = requests.get('https://api.github.com/events')
+    print(r.status_code)
+    print()
 
 
 if __name__ == "__main__":
